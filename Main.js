6ee208a077edac6e25fd65a2542e8a844df9678c
8329441266 <sadashivingale2810@gmail.com>
2020-12-27 23:31:42 -0800

    Enter email address

--- Main.js
+++ Main.js
@@ -4,4 +4,6 @@ const util = require("./Utility");
 var fName = readlineSync.question( "Enter First name : ");
 util.firstName(fName);
 var lName = readlineSync.question( "Enter Last name : ");
-util.firstName(lName);
+util.lastName(lName);
+var email = readlineSync.question( "Enter email address : ");
+util.emailAddress(email);
--- Utility.js
+++ Utility.js
@@ -1,8 +1,8 @@
 class Utility{
-     firstName(fname) {
+     firstName = (fname) => {
         let FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
 
-        if (FIRST_NAME_PATTERN.test(fname)) {
+        if (FIRST_NAME_PATTERN.test(fname)){
             console.log("valid First Name");
         }
         else{
@@ -10,7 +10,7 @@ class Utility{
         }
         return FIRST_NAME_PATTERN;
     }
-    lastName(lname) {
+    lastName = (lname) => {
         let LAST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
 
         if (LAST_NAME_PATTERN.test(lname)) {
@@ -20,6 +20,17 @@ class Utility{
             console.log("Invalid Last Name");
         }
         return LAST_NAME_PATTERN;
+    }
+    emailAddress = (email) => {
+        let EMAIL_PATTERN = new RegExp("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$");
+
+        if (EMAIL_PATTERN.test(email)) {
+            console.log("valid Email");
+        }
+        else{
+            console.log("Invalid Email");
+        }
+        return EMAIL_PATTERN;
     }
 }
 module.exports = new Utility();
