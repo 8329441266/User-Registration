865363b2b89ccf44db22d480c8fa204ef9b074e6
8329441266 <sadashivingale2810@gmail.com>
2020-12-27 23:23:13 -0800

    Enter last name

--- Main.js
+++ Main.js
@@ -3,3 +3,5 @@ var readlineSync = require('readline-sync');
 const util = require("./Utility");
 var fName = readlineSync.question( "Enter First name : ");
 util.firstName(fName);
+var lName = readlineSync.question( "Enter Last name : ");
+util.firstName(lName);
--- Utility.js
+++ Utility.js
@@ -5,11 +5,21 @@ class Utility{
         if (FIRST_NAME_PATTERN.test(fname)) {
             console.log("valid First Name");
         }
-        else {
+        else{
             console.log("Invalid First Name");
         }
         return FIRST_NAME_PATTERN;
-       
+    }
+    lastName(lname) {
+        let LAST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+
+        if (LAST_NAME_PATTERN.test(lname)) {
+            console.log("valid Last Name");
+        }
+        else{
+            console.log("Invalid Last Name");
+        }
+        return LAST_NAME_PATTERN;
     }
 }
 module.exports = new Utility();
